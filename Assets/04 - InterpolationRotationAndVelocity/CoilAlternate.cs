14f6290f23eac7f253bd01da88ba127cde9fe54b
RobertoPlinio <robertoppjr@gmail.com>
2021-07-19 01:59:34 -0300

    Started another try at exercise 9

---
+++ Assets/04 - InterpolationRotationAndVelocity/CoilAlternate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoilAlternate : MonoBehaviour
+{
+    public int densityPerTurn;
+    public float height;
+    public float radius;
+    public int turns;
+    public Color startColor;
+    public Color endColor;
+
+    const float TAU = Mathf.PI * 2f;
+    Vector3 AngToDir(float angRad) => new Vector3(Mathf.Cos(angRad), Mathf.Sin(angRad));
+    float DirToAng(Vector2 v) => Mathf.Atan2(v.y, v.x);
+
+    private void OnDrawGizmos() {
+        float fullDensity = densityPerTurn * turns;
+
+        for (int i = 0; i <= fullDensity; i++) {
+            float angRad = i * TAU / fullDensity * turns;
+            
+            //Vector3 pos = AngToDir(angRad);
+            //pos.z = i * height / fullDensity;
+            //Gizmos.DrawSphere(transform.InverseTransformPoint(pos), 0.1f);
+        }
+    }
+
+    Vector3 GetCoilLocalPosition() {
+        return default;
+    }
+}
