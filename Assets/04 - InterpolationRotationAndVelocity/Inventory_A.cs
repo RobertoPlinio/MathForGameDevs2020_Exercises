783d20911d6383a4239d79a9c6d945d8fe87d6a5
RobertoPlinio <robertoppjr@gmail.com>
2021-08-22 21:28:36 -0300

    Started 10b exercise, will do law of cosines implementation

---
+++ Assets/04 - InterpolationRotationAndVelocity/Inventory_A.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Inventory_A : MonoBehaviour
+{
+    //I will assume the reference game object this inventory
+    // will show arcs is the same this script is on
+
+    public int numberOfItems;
+    public float arcRadius;
+    public float itemRadius;
+
+    private void OnDrawGizmos() {
+        if (numberOfItems < 1) return;
+
+        float itemDifferenceAngle = Mathf.Asin(itemRadius / arcRadius) * 2;
+        //The offset to be subtracted as to make the items in the center
+        float angleOffset = itemDifferenceAngle * (numberOfItems - 1) / 2f;
+
+        for (int i = 0; i < numberOfItems; i++) {
+            float angle = i * itemDifferenceAngle - angleOffset;
+            Vector3 itemPos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)) * arcRadius;
+            Handles.DrawWireDisc(itemPos, transform.forward, itemRadius);
+        }
+    }
+}
---
+++ Assets/04 - InterpolationRotationAndVelocity/Inventory_B.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class Inventory_B : MonoBehaviour
+{
+    public float arcRadius;
+
+    public Item[] items;
+
+    private void OnDrawGizmos() {
+        float numberOfItems = items.Length;
+        if (numberOfItems < 1) return;
+
+        float itemDifferenceAngle = 0;
+        float angleOffset = 0;
+
+        for (int i = 0; i < numberOfItems; i++) {
+            itemDifferenceAngle = Mathf.Asin(items[i].radius / arcRadius);
+            if (i + 1 < numberOfItems) itemDifferenceAngle += Mathf.Asin(items[i + 1].radius / arcRadius);
+            angleOffset += itemDifferenceAngle;
+        }
+
+        angleOffset = angleOffset * (numberOfItems - 1) / 2f;
+
+        for (int i = 0; i < numberOfItems; i++) {
+            itemDifferenceAngle = Mathf.Asin(items[i].radius / arcRadius) * 2f;
+            float angle = i * itemDifferenceAngle - angleOffset;
+            Vector3 itemPos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)) * arcRadius;
+            Handles.DrawWireDisc(itemPos, transform.forward, items[i].radius);
+        }
+    }
+
+    [Serializable]
+    public struct Item
+    {
+        public float radius;
+    }
+}
